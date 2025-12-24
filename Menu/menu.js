23ff28daef636b4622610a27c8f30cc1f7f3c354
Hadixp <hadi.b.2002@gmail.com>
2025-12-25 00:03:02 +0330

    Update menu.js

--- Menu/menu.js
+++ Menu/menu.js
@@ -1,10 +1,6 @@
 // selecting icons for opening and closing menu
-// sandwitch icon
-
-
-
 let previusBox
-
+// sandwitch
 const sandwitchIcon = document.querySelector(".sandwichIcon");
 const sandwitchBox = document.querySelector(".sandwich-box");
 sandwitchIcon.addEventListener("click", () => {
@@ -14,7 +10,7 @@ sandwitchIcon.addEventListener("click", () => {
     sandwitchBox.style.display = "block";
     previusBox = sandwitchBox;
 })
-
+// pizza
 const pizzaIcon = document.querySelector(".americanPizza");
 const pizzaBox = document.querySelector(".pizzaBox");
 pizzaIcon.addEventListener("click", () => {
@@ -24,6 +20,27 @@ pizzaIcon.addEventListener("click", () => {
     pizzaBox.style.display = "block";
     previusBox = pizzaBox;
 })
+// pasta
+const pastaIcon = document.querySelector('.pasta');
+const pastaBox = document.querySelector('.pastaBox');
+pastaIcon.addEventListener("click",()=>{
+    if(previusBox){
+        previusBox.style.display = "none";
+    }
+    pastaBox.style.display = "block";
+    previusBox = pastaBox;
+})
+// glasse
+const glasseIcon = document.querySelector('.gelasse');
+const glasseBox = document.querySelector('.galsse-box');
+glasseBox.addEventListener("click",()=>{
+    if()
+})
+
+
+
+
+
 
 
 
